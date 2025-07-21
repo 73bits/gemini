9ae4b5ed321dfc9eaaf2d7168c56c78cff050eed
73bits <prashantkr0628@gmail.com>
2025-07-22 00:55:26 +0530

    initial commit

---
+++ gemini.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from google import genai
+
+def main() -> None:
+    if len(sys.argv) == 2:
+        prompt: str = sys.argv[1]
+        try:
+            client = genai.Client(api_key = os.getenv('GOOGLE_API_KEY'))
+            response = client.models.generate_content(model = 'gemini-2.5-flash', contents = prompt)
+            print(response.text.replace("**", "").replace("`", ""))
+            sys.exit(0)
+        except Exception as e:
+            print(e)
+    else:
+        print("error: require one prompt")
+        sys.exit(1)
+    return None
+
+if __name__ == "__main__":
+    main()
